cf87d745ac6f00fb5e417adbfe986ad8bf9383b3
mati5123 <mati5123@noreply.github.com>
2023-12-04 20:24:30 +0100

    templates part 2

--- hello/views.py
+++ hello/views.py
@@ -1,21 +1,29 @@
 from django.shortcuts import HttpResponse, render
 
-def hello_views(request): # request z konwencji , funkcaj musi zwraciac http
-    return HttpResponse("hello word") # dopiero widok moze byc str
+
+def hello_views(request):  # request z konwencji , funkcaj musi zwraciac http
+    return HttpResponse("hello word")  # dopiero widok moze byc str
+
 
 def hello_eva(request):
     return HttpResponse("helo Eva")
 
+
 def hello_adam(request):
     return HttpResponse("hello Adam")
+
 
 def hello_name(request, name):
     return HttpResponse(f"hello {name}")
 
+
 def hello_template(request):
-    return HttpResponse ("Hello Eva")
-
-def hello_template2(request,name):
-    return render(request,'name_template.html')
+    return HttpResponse("Hello Eva")
 
 
+def hello_template2(request, name):
+    return render(
+        request,
+        'name_template.html',  # szablon
+        context={'name': name})  # kontekst czyli kieszen
+        #context = {'x': name})  # mozemy zmienic nazwe name na x ale w html tez trzeba
